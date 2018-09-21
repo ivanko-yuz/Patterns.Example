8049bd5549757f651e9e550f756ac542c5544d53
Ivan Yuzvyshyn <iyuzv@softserveinc.com>
2018-09-21 16:50:30 +0300

    add Chain of Responsibility

---
+++ Patterns.Example.ChainOfResponsibility/Handlers/FirstHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Patterns.Example.ChainOfResponsibility
+{
+    public class FirstHandler : BaseHandler
+    {
+
+        public override void Handle(object item)
+        {
+            if(item is int)
+            {
+                Console.WriteLine($"First handler handled the item :{item} typeof: {item.GetType()}");
+            }
+            else
+            {
+                base.Handle(item);
+            }
+        }
+    }
+}
---
+++ Patterns.Example.ChainOfResponsibility/Handlers/SecondHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Patterns.Example.ChainOfResponsibility
+{
+    public class SecondHandler : BaseHandler
+    {
+
+        public override void Handle(object item)
+        {
+            if(item is char)
+            {
+                Console.WriteLine($"Second handler handled the item :{item} typeof: {item.GetType()}");
+            }
+            else
+            {
+                base.Handle(item);
+            }
+        }
+    }
+}
